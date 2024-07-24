d217c30a74083a658e97150d62527b7ef4ebf2b1
kanishkraj07 <kanishkmogalraj@gmail.com>
2024-07-24 21:25:31 +0530

    added ui changes at skill container

--- src/components/ui/DetailsBox.jsx
+++ src/components/ui/DetailsBox.jsx
@@ -2,7 +2,7 @@ const DetailsBox = ({name, icon}) => {
 
     return (
         <div className="flex flex-col place-items-center border border-[#1f1f1f] p-7 gap-3 max-w-30 max-h-40 rounded-md hover:border-slate-300">
-            <img className='w-full h-full size-14 object-cover max-h-14 max-w-14' src={icon}></img>
+            <img className='w-full h-full size-10 object-cover max-h-10 max-w-10' src={icon}></img>
             <div className='text-base text-white font-primaryRegular'>{name}</div>
         </div>
     )
