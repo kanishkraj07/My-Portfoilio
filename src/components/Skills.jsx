54eab864fce5d88696be21f044121771c285819d
kanishkraj07 <kanishkmogalraj@gmail.com>
2024-07-24 21:36:46 +0530

    added ui changes at skill container

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,7 +8,7 @@ export default function Skills() {
   return (
       <section className='flex flex-col justify-center gap-5'>
         <h1 className='text-center text-4xl font-primaryMedium text-white'>Skills</h1>
-        <ShineBorder className='relative p-5 flex flex-wrap gap-5 bg-black items-center justify-center overflow-hidden rounded-lg'  color="white">
+        <ShineBorder className='w-full h-fit relative p-5 flex flex-wrap gap-5 bg-black items-center justify-center overflow-hidden rounded-lg'  color="white">
           {SKILLS_LIST.map(skill => <DetailsBox name={skill.name} icon={skill.icon} />)}
           {/* <BorderBeam colorFrom='white' colorTo='white' duration={12} delay={9}/> */}
         </ShineBorder>
--- src/components/ui/DetailsBox.jsx
+++ src/components/ui/DetailsBox.jsx
@@ -1,8 +1,8 @@
 const DetailsBox = ({name, icon}) => {
 
     return (
-        <div className="flex flex-col place-items-center border border-[#1f1f1f] p-7 gap-3 max-w-30 max-h-40 rounded-md hover:border-slate-300">
-            <img className='w-full h-full size-10 object-cover max-h-10 max-w-10' src={icon}></img>
+        <div className="flex flex-col place-items-center border border-[#1f1f1f] p-7 gap-3 rounded-md hover:border-slate-300">
+            <img className='w-full h-full size-8 object-cover max-h-8 max-w-8' src={icon}></img>
             <div className='text-base text-white font-primaryRegular'>{name}</div>
         </div>
     )
