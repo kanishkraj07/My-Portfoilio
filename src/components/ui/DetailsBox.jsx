1c2f5faf1bd383b26ef5cfa678a66ab1811f38a8
kanishkraj07 <kanishkmogalraj@gmail.com>
2024-07-24 19:48:37 +0530

    changes skills icons

--- src/components/ui/DetailsBox.jsx
+++ src/components/ui/DetailsBox.jsx
@@ -2,7 +2,7 @@ const DetailsBox = ({name, icon}) => {
 
     return (
         <div className="flex flex-col place-items-center border border-[#1f1f1f] p-7 gap-3 max-w-30 max-h-40 rounded-md hover:border-slate-300">
-            <img className='w-full h-full size-20 object-cover max-h-20 max-w-20' src={icon}></img>
+            <img className='w-full h-full size-16 object-cover max-h-16 max-w-16' src={icon}></img>
             <div className='text-base text-white font-primaryRegular'>{name}</div>
         </div>
     )
