6bd235281cb43fceb527b29fc733412fb03150b5
kanishkraj07 <kanishkmogalraj@gmail.com>
2024-07-24 20:24:01 +0530

    added social links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -56,14 +56,14 @@ export default function Home() {
           </div>
 
           <div className='flex justify-start items-start gap-3 mt-5'>
-            <a className='text-white border-2 border-[#1f1f1f] rounded-full size-14 flex justify-center items-center hover:border-slate-300' href="https://www.linkedin.com/in/s-chandana-16a5b72b4/">
+            <a className='text-white border-2 border-[#1f1f1f] rounded-full size-14 flex justify-center items-center hover:border-slate-300' href="https://www.linkedin.com/in/kanishk777/">
               <Linkedin size={25} strokeWidth='1px' fill='white' />
             </a>
-            <a href="https://github.com/S-ChandanaSri" className='text-white border-2 border-[#1f1f1f] hover:border-slate-300 rounded-full size-14 flex justify-center items-center' >
+            <a href="https://github.com/kanishkraj07" className='text-white border-2 border-[#1f1f1f] hover:border-slate-300 rounded-full size-14 flex justify-center items-center' >
               <Github size={25} strokeWidth='1px' fill='white' />
             </a>
           
-            <a href="https://instagram.com" className='text-white border-2 border-[#1f1f1f] rounded-full hover:border-slate-300 size-14 flex justify-center items-center' >
+            <a href="https://x.com/raj_kanishk_" className='text-white border-2 border-[#1f1f1f] rounded-full hover:border-slate-300 size-14 flex justify-center items-center' >
               <Twitter size={25} strokeWidth='1px' fill='white' />
             </a>
             <ShimmerButton className='bg-slate-300'><span className='font-primaryMedium tracking-tight'>View & Download Resume</span></ShimmerButton>
--- src/components/ui/DetailsBox.jsx
+++ src/components/ui/DetailsBox.jsx
@@ -2,7 +2,7 @@ const DetailsBox = ({name, icon}) => {
 
     return (
         <div className="flex flex-col place-items-center border border-[#1f1f1f] p-7 gap-3 max-w-30 max-h-40 rounded-md hover:border-slate-300">
-            <img className='w-full h-full size-16 object-cover max-h-16 max-w-16' src={icon}></img>
+            <img className='w-full h-full size-14 object-cover max-h-14 max-w-14' src={icon}></img>
             <div className='text-base text-white font-primaryRegular'>{name}</div>
         </div>
     )
