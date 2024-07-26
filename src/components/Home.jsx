f497c8bbf838a51a65e93e71ed18203067fb7696
kanishkraj07 <kanishkmogalraj@gmail.com>
2024-07-26 23:23:20 +0530

    Added responsive home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -40,17 +40,17 @@ export default function Home() {
           </div>
         </div>
   </nav>
-  <Element name="home-section" className='grid grid-cols-1 w-full h-screen gap-7 px-5'>
-      <div className="w-full h-full flex justify-center items-center gap-10">
+  <Element name="home-section" className='grid grid-cols-1 w-full h-screen gap-7 px-5 place-items-center'>
+      <div className="w-full h-full flex flex-col-reverse lg:px-10 lg:flex-row justify-center items-center gap-5 lg:gap-10">
         <div className='leading-7 flex flex-col'>
           <div className='text-white text-2xl'>Hi, it's</div>
           <div className='text-5xl text-primary-blue mt-1 font-primaryMedium'>Kanishk Mogalraj</div>
           
           <h3 className='text-white text-2xl mt-2'>I'm a <span className="text text-white font-primaryMedium">Full Stack Engineer</span></h3>
           <div className='text-white text-md mt-5'>
-            <span>I'm a tech enthusiast, and my toolkit includes
+            <span className='break-words'>I'm a tech enthusiast, and my toolkit includes
             vibrant front-end technologies like React, Typescript,
-            Javascript, and Tailwind CSS, <br /> paired with the
+            Javascript, and Tailwind CSS, <br className='hidden lg:block' /> paired with the
             solid back-end combo of Express.js and Node.js
             </span>
           </div>
@@ -66,10 +66,11 @@ export default function Home() {
             <a href="https://x.com/raj_kanishk_" className='text-white border-2 border-[#1f1f1f] rounded-full hover:border-slate-300 size-14 flex justify-center items-center' >
               <Twitter size={25} strokeWidth='1px' fill='white' />
             </a>
-            <ShimmerButton className='bg-slate-300'><span className='font-primaryMedium tracking-tight'>View & Download Resume</span></ShimmerButton>
+
+            <a href="My_Resume.pdf" download="Kanishk Mogalraj Resume'"><ShimmerButton className='bg-slate-300 py-5'><span className='font-primaryMedium leading-none tracking-tight'>View & Download Resume</span></ShimmerButton></a>
         </div>
       </div>
-      <img className='size-80 object-cover rounded-full' src={mypic} alt="kanishk Image"/>
+      <img className='size-80  object-cover rounded-full' src={mypic} alt="kanishk Image"/>
     </div>
   </Element>
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BorderBeam } from './magicui/border-beam';
 import DetailsBox from './ui/DetailsBox';
 import { SKILLS_LIST } from '../utils';
 import ShineBorder from './magicui/shine-border';
@@ -8,9 +7,10 @@ export default function Skills() {
   return (
       <section className='flex flex-col justify-center gap-5'>
         <h1 className='text-center text-4xl font-primaryMedium text-white'>Skills</h1>
-        <ShineBorder className='w-full h-fit relative p-5 flex flex-wrap gap-5 bg-black items-center justify-center overflow-hidden rounded-lg'  color="white">
-          {SKILLS_LIST.map(skill => <DetailsBox name={skill.name} icon={skill.icon} />)}
-          {/* <BorderBeam colorFrom='white' colorTo='white' duration={12} delay={9}/> */}
+        <ShineBorder className='w-full h-fit relative p-7 bg-black rounded-lg'  color="white">
+          <div className='w-full h-fit relative flex flex-wrap gap-5 bg-black items-center justify-center rounded-lg'>
+              {SKILLS_LIST.map(skill => <DetailsBox name={skill.name} icon={skill.icon} />)}
+          </div>
         </ShineBorder>
     </section>
   )
